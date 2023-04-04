4b7460c2251fb3daf545af8095289c20ff70ac7f
Johannes Nägele <johannes.naegele@adacon-consulting.de>
2023-04-04 03:52:12 +0200

    Added List example

---
+++ src/examples/AsList.jl
@@ -0,0 +1,11 @@
+# based on Frames Catherine White's blog post at https://invenia.github.io/blog/2019/11/06/julialang-features-part-2/
+
+@traitdef IsList
+@traitimpl IsList(Type{<:AbstractVector}, Type{<:Tuple})
+
+@iftraits aslist(x::T) where T = IsList(T) ? x : [x]
+
+aslist(1)
+aslist([1,2,3])
+aslist([1])
+aslist((1, 2, 3))
