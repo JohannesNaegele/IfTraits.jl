c55d3a821f3af9b29b44c232123f636786acb6e2
Johannes Nägele <johannes.naegele@adacon-consulting.de>
2023-09-08 23:23:15 +0200

    Cleaned up basic example and added Manifest to gitignore

--- examples/Basic.jl
+++ examples/Basic.jl
@@ -1,4 +1,4 @@
-using .IfTraits
+# easiest example
 @traitdef IsNice
 @traitimpl IsNice(Int)
 @iftraits f(x) = IsNice(x) ? "Very nice!" : "Not so nice!"
@@ -6,20 +6,14 @@ using .IfTraits
 f(5)
 f(5.0)
 
+# negation works
 @iftraits f(x) = !IsNice(x) ? "Very nice!" : "Not so nice!"
 
 f(5)
 f(5.0)
 
+# Julia compiles out redundant lines of code
+@iftraits f(x) = !IsNice(x) ? 1 : 0
 
-@test f(5) == "Very nice!"
-@test f(5.0) == "Not so nice!"
-
-@code_native f(5)
+@code_llvm f(5)
 @code_native f(5.0)
-
-@traitimpl IsNice(String)
-@iftraits foo(x) = IsNice(x) ? 2 : 0
-f(5.0) == "Not so nice!"
-
-@code_native foo("5")
