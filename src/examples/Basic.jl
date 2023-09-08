b4f8f7997546b0b13943f932a33e8f9ded363d25
Johannes Nägele <johannes.naegele@adacon-consulting.de>
2023-09-08 19:10:12 +0200

    Minor changes to basic example

--- src/examples/Basic.jl
+++ src/examples/Basic.jl
@@ -1,6 +1,16 @@
+using .IfTraits
 @traitdef IsNice
 @traitimpl IsNice(Int)
 @iftraits f(x) = IsNice(x) ? "Very nice!" : "Not so nice!"
+
+f(5)
+f(5.0)
+
+@iftraits f(x) = !IsNice(x) ? "Very nice!" : "Not so nice!"
+
+f(5)
+f(5.0)
+
 
 @test f(5) == "Very nice!"
 @test f(5.0) == "Not so nice!"
@@ -10,10 +20,6 @@
 
 @traitimpl IsNice(String)
 @iftraits foo(x) = IsNice(x) ? 2 : 0
-# bar(x) = (x isa Int) || (x isa String) ? 2 : 0
+f(5.0) == "Not so nice!"
 
 @code_native foo("5")
-# @code_native bar("5")
-# using BenchmarkTools
-# @btime foo(5)
-# @btime bar(5)
